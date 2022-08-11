a9d1d8760ed7e3a3d7b9891c7c3c1102d41be603
Christian Anorga <anorga2990@gmail.com>
2022-08-10 21:53:41 -0700

    fixed router link

--- src/components/Home.js
+++ src/components/Home.js
@@ -68,14 +68,14 @@ export default function Home() {
               <nav className="relative flex items-center justify-between sm:h-10 md:justify-center" aria-label="Global">
                 <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
                   <div className="flex items-center justify-between w-full md:w-auto">
-                    <a href="#">
+                    <Link to="/">
                       <span className="sr-only">Workflow</span>
                       <img
                         className="h-12 w-auto sm:h-14"
                         src={glasses}
                         alt="logo"
                       />
-                    </a>
+                    </Link>
                     <div className="-mr-2 flex items-center md:hidden">
                       <Popover.Button className="bg-gray-50 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-500">
                         <span className="sr-only">Open main menu</span>
