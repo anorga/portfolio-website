d90591805ed26b4d218218ca3afc75a1595b05a4
Christian Anorga <anorga2990@gmail.com>
2022-08-12 10:40:18 -0700

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -185,7 +185,7 @@ export default function Projects() {
                             </div>
                             <div className="relative max-w-7xl mx-auto">
                                 <div className="text-center">
-                                    <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Projects</h2>
+                                    <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-5xl">Projects</h2>
                                     <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
                                         This is a list of personal projects that I have worked on. Links to hosted web app and repository are provided.
                                     </p>
