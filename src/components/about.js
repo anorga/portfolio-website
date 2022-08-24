7b800cf861f01e82ac69b5d603ed389200e18010
Christian Anorga <anorga2990@gmail.com>
2022-08-24 16:02:38 -0700

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -15,7 +15,7 @@ export default function About() {
                         </div>
                     </div>
                     <p className="mt-6 max-w-5xl mx-auto sm:text-lg md:text-xl md:leading-8 text-gray-500">
-                        I am a front-end developer with a passion for building web applications and a deep love for JavaScript, React, and everything related to web development. The endless combination of technology and creativity in this field is what drives my passion and excitement to keep learning and building. Please refer to the Projects section for a recent list of applications I have worked on. When I am not at my computer, I spend my time reading, watching my favorite tv shows, and keeping fit. 
+                        I am a front-end developer with a passion for building web applications and a deep love for JavaScript, React, and everything related to web development. The endless combination of technology and creativity in this field is what drives my passion and excitement to keep learning and building. Please refer to the Projects section for a recent list of applications I have worked on. When I am not at my computer, I spend my time reading, watching my favorite tv shows, and keeping fit. .
                     </p>
                 </div>
 
