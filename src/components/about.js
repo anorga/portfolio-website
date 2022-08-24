1f8bf5d039825a592fa51fb8ce908dd0d13c27d8
Christian Anorga <anorga2990@gmail.com>
2022-08-24 15:36:31 -0700

    UI changes

--- src/components/about.js
+++ src/components/about.js
@@ -11,11 +11,14 @@ export default function About() {
                             <div className="w-full border-t border-gray-300" />
                         </div>
                         <div className="relative flex justify-center">
-                            <span className="px-3 bg-white text-lg font-semibold uppercase text-gray-600">About Me</span>
+                            <span className="px-3 bg-white text-xl font-semibold uppercase text-gray-600">About Me</span>
                         </div>
                     </div>
-                    
+                    <p className="mt-6 max-w-4xl mx-auto text-gray-500">
+                        I am a full stack developer with a passion for building web applications. I have a background in.This is a list of personal projects that I have worked on. Links to hosted web app and repository are provided.This is a list of personal projects that I have worked on. Links to hosted web app and repository are provided.This is a list of personal projects that I have worked on. Links to hosted web app and repository are provided.
+                   </p>
                 </div>
+                
             </div>
         </Fragment>
     )
