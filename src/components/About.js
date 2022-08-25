af28c3e3a56b937c30e4c229fcc246460dc499ce
Christian Anorga <anorga2990@gmail.com>
2022-08-24 17:37:33 -0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import { Fragment } from "react"
+import { Link } from "react-router-dom"
 
 export default function About() {
     return (
@@ -17,7 +18,7 @@ export default function About() {
                     <p className="mt-6 max-w-5xl mx-auto sm:text-lg md:text-xl md:leading-8 text-gray-500">
                         I am a front-end developer with a passion for building web applications and a deep love for JavaScript, React, and everything related to web development. The endless combination of technology and creativity in this field is what drives my passion and excitement to keep learning and building. Please refer to the Projects section for a recent list of applications I have worked on. When I am not at my computer, I spend my time reading, watching my favorite tv shows, and keeping fit.
                     </p>
-                    
+
                 </div>
             </div>
         </Fragment>
