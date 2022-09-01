94f7fd6a1cebb1703d7c856467cfed7186f80f16
Christian Anorga <anorga2990@gmail.com>
2022-09-01 11:17:48 -0700

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,57 +9,57 @@ import TailwindLogo from './assets/tailwind.png'
 export default function Skills() {
   return (
     <Fragment>
-      <div className="bg-white">
-        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+      <div className="bg-gray-50">
+        <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:py-16 lg:px-8">
 
           <div className="relative">
             <div className="absolute inset-0 flex items-center" aria-hidden="true">
               <div className="w-full border-t border-gray-300" />
             </div>
             <div className="relative flex justify-center">
-              <span className="px-3 bg-white sm:text-lg md:text-xl font-semibold uppercase text-gray-600">Development Tools and Languages</span>
+              <span className="px-3 font-semibold text-gray-600 uppercase bg-white sm:text-lg md:text-xl">Development Tools and Languages</span>
             </div>
           </div>
 
-          {/* <p className="text-center text-base font-semibold uppercase text-gray-600 tracking-wider">
+          {/* <p className="text-base font-semibold tracking-wider text-center text-gray-600 uppercase">
             Development Tools and Languages
           </p> */}
           <div className="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
-            <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <div className="flex justify-center col-span-1 px-8 py-8 bg-gray-50">
               <img
                 className="max-h-12"
                 src={ReactLogo}
                 alt="React"
               />
-              <div className="pl-2 pt-2 text-3xl font-bold text-gray-400">React</div>
+              <div className="pt-2 pl-2 text-3xl font-bold text-gray-400">React</div>
             </div>
-            <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <div className="flex justify-center col-span-1 px-8 py-8 bg-gray-50">
               <img className="max-h-12" src={HTML5Logo} alt="HTML" />
-              <div className="pl-2 pt-2 text-3xl font-bold text-gray-400">HTML</div>
+              <div className="pt-2 pl-2 text-3xl font-bold text-gray-400">HTML</div>
             </div>
-            <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <div className="flex justify-center col-span-1 px-8 py-8 bg-gray-50">
               <img className="max-h-12" src={CSS3Logo} alt="CSS" />
-              <div className="pl-2 pt-2 text-3xl font-bold text-gray-400">CSS</div>
+              <div className="pt-2 pl-2 text-3xl font-bold text-gray-400">CSS</div>
             </div>
-            <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <div className="flex justify-center col-span-1 px-8 py-8 bg-gray-50">
               <img className="max-h-12" src={JSLogo} alt="JavaScript" />
-              <div className="pl-2 pt-2 text-3xl font-bold text-gray-400">JS</div>
+              <div className="pt-2 pl-2 text-3xl font-bold text-gray-400">JS</div>
             </div>
-            <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <div className="flex justify-center col-span-1 px-8 py-8 bg-gray-50">
               <img
                 className="max-h-12"
                 src={NodeLogo}
                 alt="Node"
               />
-              <div className="pl-2 pt-2 text-3xl font-bold text-gray-400">Node.js</div>
+              <div className="pt-2 pl-2 text-3xl font-bold text-gray-400">Node.js</div>
             </div>
-            <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <div className="flex justify-center col-span-1 px-8 py-8 bg-gray-50">
               <img
-                className="max-h-8 mt-2"
+                className="mt-2 max-h-8"
                 src={TailwindLogo}
                 alt="Tailwind"
               />
-              <div className="pl-2 pt-2 text-3xl font-bold text-gray-400">Tailwind</div>
+              <div className="pt-2 pl-2 text-3xl font-bold text-gray-400">Tailwind</div>
             </div>
           </div>
         </div>
