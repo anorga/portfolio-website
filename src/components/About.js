79d7e25f326f7f2f103bd31f79dd0cd2c8f3ef5a
Christian Anorga <anorga2990@gmail.com>
2022-12-08 13:22:58 -0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -17,11 +17,11 @@ export default function About() {
                         </div>
                     </div>
                     <div className="px-5 pt-1 pb-8 bg-gray-50 mt-7 flex">
-                        <p className="mx-auto mt-6 leading-7 text-gray-500 max-w-7xl sm:text-lg md:text-xl md:leading-9">
+                        <p className="mx-auto mt-6 px-3 leading-7 text-gray-500 max-w-7xl sm:text-lg md:text-xl md:leading-9">
                             I am a front-end developer with a passion for building web applications and a deep love for JavaScript, React, and everything related to web development. The endless combination of technology and creativity in this field drives my passion and excitement to keep learning and building. Please refer to the <Link to={'/Projects'} className="hover:text-red-700 animate-pulse">Projects</Link> section for recent applications I have worked on. When I am not at my desk, I spend my time reading, watching my favorite tv shows, and keeping fit.
                         </p>
-                        <img src={portrait} alt="portrait" className="w-1/4 h-1/4 mx-10 mt-8 rounded-xl opacity-95" />
-                        <p className="mx-auto mt-6 leading-7 text-gray-500 max-w-7xl sm:text-lg md:text-xl md:leading-9">
+                        <img src={portrait} alt="portrait" className="w-1/4 h-1/4 mx-10 mt-12 rounded-xl opacity-95 hidden lg:inline-block" />
+                        <p className="mx-auto mt-6 px-3 leading-7 text-gray-500 max-w-7xl sm:text-lg md:text-xl md:leading-9">
                             I am a front-end developer with a passion for building web applications and a deep love for JavaScript, React, and everything related to web development. The endless combination of technology and creativity in this field drives my passion and excitement to keep learning and building. Please refer to the <Link to={'/Projects'} className="hover:text-red-700 animate-pulse">Projects</Link> section for recent applications I have worked on. When I am not at my desk, I spend my time reading, watching my favorite tv shows, and keeping fit.
                         </p>
                     </div>
