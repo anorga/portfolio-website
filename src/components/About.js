4c2f4bfe085d35a8d03251cdaf9bdfbfc0d4d875
Christian Anorga <anorga2990@gmail.com>
2022-12-08 13:05:42 -0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react"
 import { Link } from "react-router-dom"
+import portrait from "./assets/portrait.jpeg"
 
 export default function About() {
     return (
@@ -19,9 +20,7 @@ export default function About() {
                         <p className="mx-auto mt-6 leading-7 text-gray-500 max-w-7xl sm:text-lg md:text-xl md:leading-9">
                             I am a front-end developer with a passion for building web applications and a deep love for JavaScript, React, and everything related to web development. The endless combination of technology and creativity in this field drives my passion and excitement to keep learning and building. Please refer to the <Link to={'/Projects'} className="hover:text-red-700 animate-pulse">Projects</Link> section for recent applications I have worked on. When I am not at my desk, I spend my time reading, watching my favorite tv shows, and keeping fit.
                         </p>
-                        <p className="mx-auto mt-6 leading-7 text-gray-500 max-w-7xl sm:text-lg md:text-xl md:leading-9">
-                            I am a front-end developer with a passion for building web applications and a deep love for JavaScript, React, and everything related to web development. The endless combination of technology and creativity in this field drives my passion and excitement to keep learning and building. Please refer to the <Link to={'/Projects'} className="hover:text-red-700 animate-pulse">Projects</Link> section for recent applications I have worked on. When I am not at my desk, I spend my time reading, watching my favorite tv shows, and keeping fit.
-                        </p>
+                        <img src={portrait} alt="portrait" className="w-1/4 h-1/4 mx-10 mt-8 rounded-xl opacity-95" />
                         <p className="mx-auto mt-6 leading-7 text-gray-500 max-w-7xl sm:text-lg md:text-xl md:leading-9">
                             I am a front-end developer with a passion for building web applications and a deep love for JavaScript, React, and everything related to web development. The endless combination of technology and creativity in this field drives my passion and excitement to keep learning and building. Please refer to the <Link to={'/Projects'} className="hover:text-red-700 animate-pulse">Projects</Link> section for recent applications I have worked on. When I am not at my desk, I spend my time reading, watching my favorite tv shows, and keeping fit.
                         </p>
