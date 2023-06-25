0b07950b01b3d84a8d0c3d92e906f4f4a89636df
Christian Anorga <anorga2990@gmail.com>
2023-06-24 23:19:32 -0700

    home UI changes

--- src/components/About.js
+++ src/components/About.js
@@ -7,27 +7,22 @@ export default function About() {
         <Fragment>
             <div className="bg-gray-50">
                 <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:py-16 lg:px-8">
-                    <div className="flex gap-40 px-6">
-                        <img src={portrait} alt="portrait" className="w-96 h-full rounded-xl opacity-95 hidden lg:block" />
+                    <div className="flex gap-40 pl-10">
+                        <img src={portrait} alt="portrait" className="w-96 h-full rounded-[60px] opacity-95 hidden lg:block" />
                         <div className="my-auto">
-                            <p><span className="text-[100px]">Hello!</span><br></br>
+                            <p><span className="text-[120px]">Hello!</span><br></br>
                             <span className="text-[50px]">I'm Chris, a Web Developer based in San Diego.</span>
                             </p>
                         </div>
                     </div>
-                    <div className="relative mt-20">
-                        <div className="absolute inset-0 flex items-center" aria-hidden="true">
-                            <div className="w-full" />
-                        </div>
-                        <div className="relative flex justify-center">
-                            <span className="px-3 font-semibold text-black uppercase bg-gray-50 sm:text-lg md:text-2xl">About Me</span>
-                        </div>
+                    <div className="mt-40 pl-10">
+                            <span className="font-semibold text-black uppercase bg-gray-50 sm:text-lg md:text-[40px]">About Me</span>
                     </div>
                     <div className="px-5 mt-5 bg-gray-50 flex flex-col lg:flex-row">
-                        <p className="mx-auto mt-6 px-3 md:px-4 leading-7 text-black max-w-7xl sm:text-lg md:text-xl md:leading-9">
-                            Hello there! I am a front-end developer with a passion for building web applications and a deep interest in everything related to web development. The endless combination of technology and creativity in this field drives my passion and excitement to keep learning and building. Please refer to the <Link to={'/Projects'} className="hover:text-red-700 animate-pulse">Projects</Link> section for recent applications I have worked on.
+                        <p className="mx-auto mt-6 px-3 md:px-4 leading-7 text-black max-w-7xl sm:text-lg md:text-2xl md:leading-9">
+                            I am a front-end developer with a passion for building web applications and a deep interest in everything related to web development. The endless combination of technology and creativity in this field drives my passion and excitement to keep learning and building. Please refer to the <Link to={'/Projects'} className="hover:text-red-700 animate-pulse">Projects</Link> section for recent applications I have worked on.
                         </p>
-                        <p className="mx-auto mt-6 px-3 md:pl-7 leading-7 text-black max-w-7xl sm:text-lg md:text-xl md:leading-9">
+                        <p className="mx-auto mt-6 px-3 md:pl-7 leading-7 text-black max-w-7xl sm:text-lg md:text-2xl md:leading-9">
                             I currently provide services to clients needing end-to-end web solutions for their organizations and deliver projects that frequently exceed expectations. Building web applications for different needs and experiences with languages, frameworks, and libraries, such as the MERN web stack, and content management systems such as WordPress.
                         </p>
                     </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,40 +12,31 @@ export default function Skills() {
       <div className="bg-gray-50">
         <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:py-16 lg:px-8">
 
-          <div className="relative mb-14">
-            <div className="absolute inset-0 flex items-center" aria-hidden="true">
-              <div className="w-full" />
-            </div>
-            <div className="relative flex justify-center">
-              <span className="px-3 font-semibold text-black uppercase bg-gray-50 sm:text-lg md:text-2xl">Development Tools and Languages</span>
-            </div>
+          <div className="mb-16 mt-10 pl-10">
+            <div className="font-semibold text-black uppercase bg-gray-50 sm:text-lg md:text-[40px]">Development Tools and Languages</div>
           </div>
 
           {/* <p className="text-base font-semibold tracking-wider text-center text-gray-600 uppercase">
             Development Tools and Languages
           </p> */}
-          <div className="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
-            <div className="flex justify-center col-span-1 px-8 py-8 bg-red-600">
-              <img
-                className="max-h-12"
-                src={ReactLogo}
-                alt="React"
-              />
+          <div className="px-10 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
+            <div className="flex justify-center col-span-1 px-8 py-8 rounded-lg bg-red-600">
+              <img className="max-h-12" src={ReactLogo} alt="React" />
               <div className="pt-2 pl-2 text-3xl font-bold text-white">React</div>
             </div>
-            <div className="flex justify-center col-span-1 px-8 py-8 bg-red-600">
+            <div className="flex justify-center col-span-1 px-8 py-8 rounded-lg bg-red-600">
               <img className="max-h-12" src={HTML5Logo} alt="HTML" />
               <div className="pt-2 pl-2 text-3xl font-bold text-white">HTML</div>
             </div>
-            <div className="flex justify-center col-span-1 px-8 py-8 bg-red-600">
+            <div className="flex justify-center col-span-1 px-8 py-8 rounded-lg bg-red-600">
               <img className="max-h-14" src={CSS3Logo} alt="CSS" />
               <div className="pt-2 pl-2 text-3xl font-bold text-white">CSS</div>
             </div>
-            <div className="flex justify-center col-span-1 px-8 py-8 bg-red-600">
+            <div className="flex justify-center col-span-1 px-8 py-8 rounded-lg bg-red-600">
               <img className="max-h-12" src={JSLogo} alt="JavaScript" />
               <div className="pt-2 pl-2 text-3xl font-bold text-white">JS</div>
             </div>
-            <div className="flex justify-center col-span-1 px-8 py-8 bg-red-600">
+            <div className="flex justify-center col-span-1 px-8 py-8 rounded-lg bg-red-600">
               <img
                 className="max-h-12"
                 src={NodeLogo}
@@ -53,7 +44,7 @@ export default function Skills() {
               />
               <div className="pt-2 pl-2 text-3xl font-bold text-white">Node.js</div>
             </div>
-            <div className="flex justify-center col-span-1 px-8 py-8 bg-red-600">
+            <div className="flex justify-center col-span-1 px-8 py-8 rounded-lg bg-red-600">
               <img
                 className="mt-2 max-h-8"
                 src={TailwindLogo}
