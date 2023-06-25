ff352e204dede20cd67919b2da0b787d75ab4a52
Christian Anorga <anorga2990@gmail.com>
2023-06-24 21:05:02 -0700

    about me copy updated

--- src/components/About.js
+++ src/components/About.js
@@ -18,7 +18,7 @@ export default function About() {
                     </div>
                     <div className="px-5 mt-7 bg-gray-50 flex flex-col lg:flex-row">
                         <p className="mx-auto mt-6 px-3 md:px-4 leading-7 text-black max-w-7xl sm:text-lg md:text-xl md:leading-9">
-                            I am a front-end developer with a passion for building web applications and a deep interest in everything related to web development. The endless combination of technology and creativity in this field drives my passion and excitement to keep learning and building. Please refer to the <Link to={'/Projects'} className="hover:text-red-700 animate-pulse">Projects</Link> section for recent applications I have worked on.
+                            Hello there! I am a front-end developer with a passion for building web applications and a deep interest in everything related to web development. The endless combination of technology and creativity in this field drives my passion and excitement to keep learning and building. Please refer to the <Link to={'/Projects'} className="hover:text-red-700 animate-pulse">Projects</Link> section for recent applications I have worked on.
                         </p>
                         <img src={portrait} alt="portrait" className="w-72 h-1/4 mx-auto mt-8 rounded-xl opacity-95 hidden lg:block" />
                         <p className="mx-auto mt-6 px-3 md:pl-7 leading-7 text-black max-w-7xl sm:text-lg md:text-xl md:leading-9">
