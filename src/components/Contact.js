f4bf85d6433264dd627d31fe18ae43e50fd3fbcf
Christian Anorga <anorga2990@gmail.com>
2024-07-08 20:14:07 -0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -114,7 +114,7 @@ export default function Home() {
                   <div className="flex items-center justify-between px-5 pt-4">
                     <div>
                       <img
-                        className="w-auto h-13"
+                        className="w-auto h-12"
                         src= {glasses}
                         alt="logo"
                       />
